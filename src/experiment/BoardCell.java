eec482eb7e3f3d1ad426553223542b70024f5e7e
Austin Purdy <abpurdy@mines.edu>
2019-10-01 10:46:32 -0600

    initialize grid

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -2,11 +2,13 @@ package experiment;
 
 public class BoardCell {
 	
-	public BoardCell(int row, int column) {
+	public BoardCell(int row, int column, String roomType) {
 		super();
 		this.row = row;
 		this.column = column;
+		this.roomType = roomType;
 	}
 	int row;
 	int column;
+	String roomType;
 }
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,14 +1,29 @@
 package experiment;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 public class IntBoard {
-	private BoardCell[][] grid;
+	private final int ROW_NUM = 25;
+	private final int COL_NUM = 24;
+	private BoardCell[][] grid = new BoardCell[ROW_NUM][COL_NUM];
 	private Map<BoardCell, Set<BoardCell> > adjMtx = new HashMap();
 	
-	private void calcAdjacencies(){
+	private void calcAdjacencies() throws IOException{
+		BufferedReader csvReader = new BufferedReader(new FileReader("Clue Board.csv"));
+		
+		for(int i = 0; i < ROW_NUM; i++) {
+			String row = csvReader.readLine();
+		    String[] data = row.split(",");
+		    for(int j = 0; j < data.length; j++) {
+		    	grid[i][j] = new BoardCell(i,j,data[j]);
+		    }
+		}
+		
 		
 	}
 	public Set<BoardCell> getAdjList(BoardCell cell){
