50b72b64989690f336718a453d59b6f604a418d7
Austin Purdy <abpurdy@mines.edu>
2019-10-01 10:27:56 -0600

    initial class setup

--- src/experiment/BoardCell.java
+++ src/experiment/BoardCell.java
@@ -1,5 +1,12 @@
 package experiment;
 
 public class BoardCell {
-
+	
+	public BoardCell(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	int row;
+	int column;
 }
--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -1,5 +1,17 @@
 package experiment;
 
-public class IntBoard {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
+public class IntBoard {
+	private BoardCell[][] grid;
+	private Map<BoardCell, Set<BoardCell> > adjMtx = new HashMap();
+	
+	private void calcAdjacencies(){
+		
+	}
+	public Set<BoardCell> getAdjList(BoardCell cell){
+		return adjMtx.get(cell);
+	}
 }
