b6c0f422fd9544a3ac0c1af138d25732954af3db
Gbagecol <54816222+Gbagecol@users.noreply.github.com>
2019-10-08 10:38:43 -0600

    Created Board class skeleton

---
+++ src/clueGame/Board.java
@@ -0,0 +1,109 @@
+package clueGame;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Board {
+	
+	
+	//class variables
+	
+	/**The maximum board width and height.*/
+	public static int MAX_BOARD_SIZE = 50;
+	/**Number of rows in the board.*/
+	private int numRows;
+	/**Number of columns in the board.*/
+	private int numColumns;
+	/**The name of the board configuration file.*/
+	private String boardConfigFile;
+	/**The name of the room configuration file.*/
+	private String roomConfigFile;
+	/**The singleton instance of this Board class.*/
+	private static Board instance = new Board();
+	/**The room legend.*/
+	private Map<Character, String> legend;
+	/**The list of adjacent cells for each cell on the board.*/
+	private Map<BoardCell, Set<BoardCell>> adjMatrix;
+	/**The current spaces that the player can move to.*/
+	private Set<BoardCell> targets;
+	/**The game board.*/
+	BoardCell[][] board;
+
+	
+	//constructor
+	
+	private Board() {
+		
+		//initialize class variables
+		
+		numRows = 0;
+		numColumns = 0;
+		boardConfigFile = "";
+		roomConfigFile = "";
+		legend = new HashMap<Character, String>();
+		adjMatrix = new HashMap<BoardCell, Set<BoardCell>>();
+		targets = new HashSet<BoardCell>();
+		board = new BoardCell[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+
+	}
+	
+	
+	//class methods
+	
+	/**Initialize the game board.*/
+	public void initialize() {}
+	
+	/**Load the room configuration from the room config file.*/
+	public void loadRoomConfig() {}
+	
+	/**Load the board configuration from the board config file.*/
+	public void loadBoardConfig() {}
+	
+	/**Load the adjacency matrix for each cell in the board.*/
+	public void calcAdjacencies() {}
+	
+	/**Calculate the current targets the player can move to.
+	 * @param cell The cell the player is on.
+	 * @param pathLength The amount of spaces the player can move.*/
+	public void calcTargets(BoardCell cell, int pathLength) {}
+
+
+	//getters and setters
+	
+	public int getNumRows() {
+		return numRows;
+	}
+
+
+	public int getNumColumns() {
+		return numColumns;
+	}
+
+
+	public static Board getInstance() {
+		return instance;
+	}
+
+
+	public Map<BoardCell, Set<BoardCell>> getAdjMatrix() {
+		return adjMatrix;
+	}
+
+
+	public Set<BoardCell> getTargets() {
+		return targets;
+	}
+	
+	public Map<Character, String> getLegend() {
+		return legend;
+	}
+
+
+	public void setConfigFiles(String boardConfigFile, String roomConfigFile) {
+		this.boardConfigFile = boardConfigFile;
+		this.roomConfigFile = roomConfigFile;
+	}
+
+}
---
+++ src/clueGame/BoardCell.java
@@ -0,0 +1,48 @@
+package clueGame;
+
+/**Class for a cell that represents a space on the game board.*/
+public class BoardCell {
+	
+	//class variables
+	
+	/**The row of this cell.*/
+	int row;
+	/**The column of this cell*/
+	int column;
+	/**The room this cell is in.*/
+	String roomType;
+	
+	
+	//constructor
+	
+	/**Create a new board cell with the given room type at the given row and column.*/
+	public BoardCell(int row, int column, String roomType) {
+		super();
+		this.row = row;
+		this.column = column;
+		this.roomType = roomType;
+	}
+	
+	
+	//getters and setters
+	
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	public String getRoomType() {
+		return roomType;
+	}
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+
+}
