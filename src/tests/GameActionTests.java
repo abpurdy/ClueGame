808c11dffe09509e29eec03f2e8c660cb6fbdee6
Gbagecol <54816222+Gbagecol@users.noreply.github.com>
2019-10-31 09:28:42 -0600

    Created action test skeletons

---
+++ src/tests/GameActionTests.java
@@ -0,0 +1,44 @@
+package tests;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+
+public class GameActionTests {
+	
+	private Board board;
+
+	@BeforeClass
+	public static void setup() {
+		
+		//TODO setup board object
+		
+	}
+	
+	@Test
+	public void testCPUTargetSelection() {
+		
+	}
+	
+	@Test
+	public void testAccusationCheck() {
+		
+	}
+	
+	@Test
+	public void testDisproveSuggestion() {
+		
+	}
+	
+	@Test
+	public void testSuggestionHandling() {
+		
+	}
+	
+	@Test
+	public void testCreateSuggestion() {
+		
+	}
+
+}
